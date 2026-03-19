0270a06cc677659a4cd68e2a8eafa23df446b677
bonijrilus16-prog <bonijr.ilus16@gmail.com>
2026-03-19 13:29:50 +0800

    Fix package declaration and class name in Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,10 @@
+//sample file
+public class main
+package Test;
+import chapter1.Toy;
 
+public class UserTest {
+    public static void main(String[] args) {
+        Toy toy1 = new Toy();
+    }
+}
